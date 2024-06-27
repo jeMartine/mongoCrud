42dc02c4d3bc52c903475a5f972cece5b550fcc4
jeMartine <jorgeesteban464@gmail.com>
2024-06-27 13:12:22 -0500

    feature: handdle format error

--- basic-CRUD-mongo/src/main/java/com/exercise/basic_CRUD_mongo/global/dto/MessageDto.java
+++ basic-CRUD-mongo/src/main/java/com/exercise/basic_CRUD_mongo/global/dto/MessageDto.java
@@ -1,5 +1,15 @@
 package com.exercise.basic_CRUD_mongo.global.dto;
 
-public class MessageDto {
+import org.springframework.http.HttpStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDto {
+    private HttpStatus status;
+    private String message;
 }
---
+++ basic-CRUD-mongo/src/main/java/com/exercise/basic_CRUD_mongo/global/exceptions/GlobalExceptions.java
@@ -0,0 +1,17 @@
+package com.exercise.basic_CRUD_mongo.global.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.exercise.basic_CRUD_mongo.global.dto.MessageDto;
+
+@RestControllerAdvice
+public class GlobalExceptions {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<MessageDto> throwNotFoundException(ResourceNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new MessageDto(HttpStatus.NOT_FOUND, e.getMessage())); 
+    } 
+}
